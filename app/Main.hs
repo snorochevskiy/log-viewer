c87a83531c5a28f495930f4d5253503aa02ae2f7
Stanislav Norochevskiy <stas.norochevskiy@gmail.com>
2017-09-16 21:21:41 +0300

    Added scotty

--- app/Main.hs
+++ app/Main.hs
@@ -1,36 +1,71 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
-import Data.List
+import System.Environment
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Encoding as TE
+
+import qualified Data.List as L
 import Data.Conduit
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.Conduit.Binary as C
+import qualified Data.Conduit.Combinators as CC
 import Control.Monad.Trans.Resource
 import Control.Monad.IO.Class
 import Data.Monoid
 
+import Web.Scotty
+import Network.HTTP.Types
+
+-- |Conduit that processes log lines as log entries.
+-- Log entry starts with a timestamp and can have trailing lines
+-- that start from any symbol except timestamp.
+-- If line starts with timestamp and contains given search string, then is passed by.
+-- If line doesnt start from timestamp (just a next line of a log entry) and previouls line
+-- relates to an entry that contains search string , then it is also passed by.
+-- 'found' indicates if previouls line relates to a log entry that contains search string
+-- 'searchString' is a search string
 logBlocks :: (Monad m, MonadIO m) => Bool -> String -> Conduit BS.ByteString m BS.ByteString
-logBlocks found srch = do
+logBlocks found searchString = do
   client <- await
   case client of
     Nothing -> return ()
     Just c -> do
       let unpaked = BS.unpack c
-      if match found unpaked srch
+      if match found unpaked searchString
         then do
           yield $ c <> BS.pack "\n"
-          logBlocks True srch
+          logBlocks True searchString
         else
-          logBlocks False srch
+          logBlocks False searchString
 
 
--- Check if starts with timestamp and contains given substring
+-- |Checks  if given line should be included into resulting log
 match :: Bool -> String -> String -> Bool
-match found str srch = (found && (length str > 0) && ((head str < '0') || (head str > '9'))) || isInfixOf srch str
+match found line searchingStr = (found && isNewLogEntry line) || L.isInfixOf searchingStr line
+    where
+      isNewLogEntry :: String -> Bool
+      isNewLogEntry line' =  (L.length line' > 0) && ((L.head line' < '0') || (L.head line' > '9'))
+
+findLogsWithIncident :: (MonadResource m, MonadIO m) => FilePath -> String -> m BS.ByteString
+findLogsWithIncident logFile searchString =
+  (C.sourceFile logFile)
+    $= C.lines
+    $= (logBlocks False (searchString))
+    $$ CC.fold
 
 main :: IO ()
 main = do
-  let source = C.sourceFile "/home/stas/Dropbox/Dropbox/workflow.log"
-      sink   = C.sinkFile   "dst.txt"
-  runResourceT $ source $= C.lines $= (logBlocks False "Found cycle for field 'market' in type 'Site' for path 'site'") $$ sink
+  params@(port:logFile:[]) <- getArgs
+  scotty (read port) $ do
+    get "/incident/:incidentId" $ do
+      (incidentId :: String) <- param "incidentId"
+      resp <- liftIO $ runResourceT $ findLogsWithIncident logFile ("incident:" ++ incidentId)
+      html $ mconcat ["<h1>Logs with incidentId:", T.pack incidentId, "</h1>\n\n", "<pre>", T.fromStrict $ TE.decodeUtf8 resp, "</pre>"]
+
+    notFound $ do
+      status notFound404
+      html "<h1>Not such page</h1>"
