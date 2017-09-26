8fb897ca5e3d501a2c1b0a6b575118b72e3a7e9d
Stas Norochevskiy <stas.norochevskiy@gmail.com>
2017-09-26 04:35:02 +0300

    Breaking ByteString to Log blocks, not to lines

--- app/Main.hs
+++ app/Main.hs
@@ -9,10 +9,12 @@ import qualified Data.Text.Lazy as T
 import qualified Data.Text.Encoding as TE
 
 import qualified Data.List as L
+import Conduit
 import Data.Conduit
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.Conduit.Binary as C
 import qualified Data.Conduit.Combinators as CC
+import Control.Monad (unless, when)
 import Control.Monad.Trans.Resource
 import Control.Monad.IO.Class
 import Data.Monoid
@@ -53,9 +55,65 @@ match found line searchingStr = (found && isNewLogEntry line) || L.isInfixOf sea
 findLogsWithIncident :: (MonadResource m, MonadIO m) => FilePath -> String -> m BS.ByteString
 findLogsWithIncident logFile searchString =
   (C.sourceFile logFile)
-    $= C.lines
-    $= (logBlocks False (searchString))
+    $= logEntries2
+--    $= C.lines
+--    $= (logBlocks False (searchString))
     $$ CC.fold
+
+logEntries :: (Monad m, MonadIO m) => Conduit BS.ByteString m BS.ByteString
+logEntries = do
+  bytes <- await
+  case bytes of
+    Nothing -> return ()
+    Just c -> do
+      yield $ BS.pack "\n\n\n" <> c <> BS.pack "\n\n\n"
+      logEntries
+
+
+-- altered "lines" function from conduit-extra
+logEntries2 :: Monad m => Conduit BS.ByteString m BS.ByteString
+logEntries2 =
+    loop []
+  where
+    loop acc = await >>= maybe (finish acc) (go acc)
+
+    finish acc =
+        let final = BS.concat $ reverse acc
+         in unless (BS.null final) (yield final)
+
+    go acc more =
+        case BS.uncons second of
+            Just (_, second') -> yield (BS.concat $ reverse $ (first:acc ++ [BS.pack "\n"])) >> go [] second'
+            Nothing -> loop $ more:acc
+      where
+        (first, second) = takeBlock more
+
+takeBlock :: BS.ByteString -> (BS.ByteString, BS.ByteString)
+takeBlock bs = takeFirestLine bs
+  where
+    takeFirestLine bs' =
+      let
+        (line, rest) = BS.breakSubstring (BS.pack "\n") bs'
+        (nextLines, nextRest) = takeNextLine rest
+      in (line <> nextLines, nextRest)
+    takeNextLine bs' =
+      case BS.uncons bs' of
+        Just (_, lTrimmed) ->
+          if isNewLogLine lTrimmed
+            then
+              let
+                (line, rest) = BS.breakSubstring (BS.pack "\n") lTrimmed
+                (nextLines, nextRest) = takeNextLine rest
+              in (line <> nextLines, nextRest)
+            else
+              (BS.pack "", bs')
+        Nothing -> (BS.pack "", BS.pack "")
+
+isNewLogLine :: BS.ByteString -> Bool
+isNewLogLine bs =
+  case BS.uncons bs of
+    Just (first, rest) -> (first >= '0' && first <= '9')
+    Nothing -> False
 
 main :: IO ()
 main = do
