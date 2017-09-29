2d7b205279d6450f40f308349241fcb86bf57f40
Stas Norochevskiy <stas.norochevskiy@gmail.com>
2017-09-29 04:21:05 +0300

    Finished log blocks splitter

--- app/Main.hs
+++ app/Main.hs
@@ -22,57 +22,28 @@ import Data.Monoid
 import Web.Scotty
 import Network.HTTP.Types
 
--- |Conduit that processes log lines as log entries.
--- Log entry starts with a timestamp and can have trailing lines
--- that start from any symbol except timestamp.
--- If line starts with timestamp and contains given search string, then is passed by.
--- If line doesnt start from timestamp (just a next line of a log entry) and previouls line
--- relates to an entry that contains search string , then it is also passed by.
--- 'found' indicates if previouls line relates to a log entry that contains search string
--- 'searchString' is a search string
-logBlocks :: (Monad m, MonadIO m) => Bool -> String -> Conduit BS.ByteString m BS.ByteString
-logBlocks found searchString = do
-  client <- await
-  case client of
-    Nothing -> return ()
-    Just c -> do
-      let unpaked = BS.unpack c
-      if match found unpaked searchString
+logFiltrationConduit :: MonadResource m => FilePath -> String -> ConduitM a c m BS.ByteString
+logFiltrationConduit logFile searchString =
+  C.sourceFile logFile
+  .| splitToLogEntries
+  .| filerLogBlocks searchString
+  .| CC.fold
+
+filerLogBlocks :: (Monad m, MonadIO m) => String -> Conduit BS.ByteString m BS.ByteString
+filerLogBlocks searchString = do
+  block <- await
+  case block of
+    Just txt ->
+      if BS.isInfixOf (BS.pack searchString) txt
         then do
-          yield $ c <> BS.pack "\n"
-          logBlocks True searchString
-        else
-          logBlocks False searchString
-
-
--- |Checks  if given line should be included into resulting log
-match :: Bool -> String -> String -> Bool
-match found line searchingStr = (found && isNewLogEntry line) || L.isInfixOf searchingStr line
-    where
-      isNewLogEntry :: String -> Bool
-      isNewLogEntry line' =  (L.length line' > 0) && ((L.head line' < '0') || (L.head line' > '9'))
-
-findLogsWithIncident :: (MonadResource m, MonadIO m) => FilePath -> String -> m BS.ByteString
-findLogsWithIncident logFile searchString =
-  (C.sourceFile logFile)
-    $= logEntries2
---    $= C.lines
---    $= (logBlocks False (searchString))
-    $$ CC.fold
-
-logEntries :: (Monad m, MonadIO m) => Conduit BS.ByteString m BS.ByteString
-logEntries = do
-  bytes <- await
-  case bytes of
+          yield txt
+          filerLogBlocks searchString
+        else filerLogBlocks searchString
     Nothing -> return ()
-    Just c -> do
-      yield $ BS.pack "\n\n\n" <> c <> BS.pack "\n\n\n"
-      logEntries
-
 
 -- altered "lines" function from conduit-extra
-logEntries2 :: Monad m => Conduit BS.ByteString m BS.ByteString
-logEntries2 =
+splitToLogEntries :: Monad m => Conduit BS.ByteString m BS.ByteString
+splitToLogEntries =
     loop []
   where
     loop acc = await >>= maybe (finish acc) (go acc)
@@ -83,7 +54,7 @@ logEntries2 =
 
     go acc more =
         case BS.uncons second of
-            Just (_, second') -> yield (BS.concat $ reverse $ (first:acc ++ [BS.pack "\n"])) >> go [] second'
+            Just (_, second') -> yield (BS.concat $ reverse $ (first:acc ++ [BS.pack "\n"]) ) >> go [] second'
             Nothing -> loop $ more:acc
       where
         (first, second) = takeBlock more
@@ -98,15 +69,15 @@ takeBlock bs = takeFirestLine bs
       in (line <> nextLines, nextRest)
     takeNextLine bs' =
       case BS.uncons bs' of
-        Just (_, lTrimmed) ->
+        Just (newLineChar, lTrimmed) ->
           if isNewLogLine lTrimmed
             then
+              (BS.pack "", bs')
+            else
               let
                 (line, rest) = BS.breakSubstring (BS.pack "\n") lTrimmed
                 (nextLines, nextRest) = takeNextLine rest
-              in (line <> nextLines, nextRest)
-            else
-              (BS.pack "", bs')
+              in (BS.pack "\n" <> line <> nextLines, nextRest)
         Nothing -> (BS.pack "", BS.pack "")
 
 isNewLogLine :: BS.ByteString -> Bool
@@ -121,7 +92,7 @@ main = do
   scotty (read port) $ do
     get "/incident/:incidentId" $ do
       (incidentId :: String) <- param "incidentId"
-      resp <- liftIO $ runResourceT $ findLogsWithIncident logFile ("incident:" ++ incidentId)
+      resp <- liftIO $ runConduitRes $ logFiltrationConduit logFile ("incident:" ++ incidentId)
       html $ mconcat ["<h1>Logs with incidentId:", T.pack incidentId, "</h1>\n\n", "<pre>", T.fromStrict $ TE.decodeUtf8 resp, "</pre>"]
 
     notFound $ do
